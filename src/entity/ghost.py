21639834559804e2b9ca310157b833394b3dbeb3
leonardecavele <leonard.decavele@gmail.com>
2026-03-25 12:15:27 +0100

    new: manhattan, best direction function and euclidean function

--- src/entity/ghost.py
+++ src/entity/ghost.py
@@ -17,13 +17,29 @@ class Ghost(Entity):
         self.pac_man: Pac_man = pac_man
 
     def next_direction(self, target: vec2) -> vec2:
-        return (0, 0)
+        best_direction: vec2 = (0, 0)
+        best_score: int = 0
+
+        for direction in Maze.Direction:
+            new_x: int = self.maze_pos[0] + direction.value[0]
+            new_y: int = self.maze_pos[1] + direction.value[1]
+            score: int = self.manhattan((new_x, new_y), target)
+
+            if best_score is None or score < best_score:
+                best_score = score
+                best_direction = direction.value
+
+        return best_direction
 
     @staticmethod
-    def pos_distance(pos1: vec2, pos2: vec2) -> int:
+    def euclidean(pos1: vec2, pos2: vec2) -> int:
         dx: int = pos2[0] - pos1[0]
         dy: int = pos2[1] - pos1[1]
         return int(sqrt(dx * dx + dy * dy))
+
+    @staticmethod
+    def manhattan(pos1: vec2, pos2: vec2) -> int:
+        return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
 
 
 class Blinky(Ghost):
@@ -46,7 +62,7 @@ class Pinky(Ghost):
 class Clyde(Ghost):
     def update(self) -> None:
         direction: vec2
-        if self.pos_distance(self.maze_pos, self.pac_man.maze_pos) <= 8:
+        if self.euclidean(self.maze_pos, self.pac_man.maze_pos) <= 8:
             direction = self.next_direction((self.maze.height, 0))
         else:
             direction = self.next_direction(self.pac_man.maze_pos)
